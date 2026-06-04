7c615684fae133ae8924be3086fbde3a65773f07
Toyota <2026063@fsc-educ.com>
2026-06-04 14:33:16 +0900

    ページの作成

--- src/main/java/com/example/blog/BlogController.java
+++ src/main/java/com/example/blog/BlogController.java
@@ -1,19 +1,32 @@
 package com.example.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
 public class BlogController {
 
 
-    @GetMapping("/")
-    public String home() {
-        return "home";
-    }
+    // @GetMapping("/")
+    // public String home(Model model) {
+    //     List<Post>posts = postService.findAll();
+    //     model.addAttribute("posts", posts);
+    //     return "home";
+    // }
 
+    // @GetMapping("/post/{id}")
+    // public String derail(@PathVariable Long id, Model model) {
+    //     Post post = postService.findById(id);
+    //     model.addAttribute("post", post);
+    //     return "detail";
+    // }
+    
     @GetMapping("/search")
     public String search() {
         return "search";
